6964e7bf75b3db2c1115ddd5d53602aa1c73bbd3
Devbluddy <saiminhhieu0712@gamil.com>
2024-01-18 09:21:45 +0700

    lab3

---
+++ lab3/app/src/main/java/com/example/a211110642248_saiminhhieu_lab3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a211110642248_saiminhhieu_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
