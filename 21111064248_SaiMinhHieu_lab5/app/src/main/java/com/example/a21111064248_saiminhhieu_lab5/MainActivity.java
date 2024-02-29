fcccef529b845523464e61e49a8481f44d2b875a
Devbluddy <saiminhhieu0712@gamil.com>
2024-02-29 09:17:45 +0700

    lab5

---
+++ 21111064248_SaiMinhHieu_lab5/app/src/main/java/com/example/a21111064248_saiminhhieu_lab5/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.a21111064248_saiminhhieu_lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinnerPerson;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinnerPerson = findViewById(R.id.spinnerPerson);
+
+        List<Person> listPerson = new ArrayList<>();
+        listPerson.add(new Person(R.drawable.avata,"Nguoi thu 1"));
+        listPerson.add(new Person(R.drawable.avata,"Nguoi thu 2"));
+        listPerson.add(new Person(R.drawable.avata,"Nguoi thu 3"));
+        listPerson.add(new Person(R.drawable.avata,"Nguoi thu 4"));
+
+        PersonAdapter personAdapter = new PersonAdapter(this,R.layout.spinner_item,listPerson);
+
+        spinnerPerson.setAdapter(personAdapter);
+
+        spinnerPerson.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Person seclectedPerson = personAdapter.getItem(position);
+                if(seclectedPerson != null){
+                    String seclectedName = seclectedPerson.getNamePerson();
+                    Toast.makeText(MainActivity.this, "Ban da chon "+seclectedName,Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
